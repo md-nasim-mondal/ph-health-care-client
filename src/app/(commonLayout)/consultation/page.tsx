13a80fcc3e4ffcd3733e3734cb378e9ae9cc34a3
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-11-06 20:35:23 +0600

    make all public page folder name

---
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -0,0 +1,9 @@
+const ConsultationPage = () => {
+  return (
+    <div>
+      <h3 className='text-3xl'>This is ConsultationPage</h3>
+    </div>
+  );
+};
+
+export default ConsultationPage;
---
+++ src/app/(commonLayout)/doctors/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DoctorDetailsPage = () => {
+    return (
+        <div>
+            <h3 className="text-3xl">This is DoctorDetailsPage</h3>
+        </div>
+    );
+};
+
+export default DoctorDetailsPage;
