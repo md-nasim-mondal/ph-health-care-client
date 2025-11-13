242096ea51b82586fdef4a8ba3f959cc031b7afb
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-11-13 13:53:06 +0600

    make logout user server function and logout button

---
+++ src/components/shared/LogoutButton.tsx
@@ -0,0 +1,14 @@
+"client component";
+
+import { logoutUser } from "@/services/auth/logoutUser";
+import { Button } from "../ui/button";
+
+const LogoutButton = () => {
+  const handleLogout = async () => {
+    await logoutUser();
+  };
+
+  return <Button onClick={handleLogout}>Logout</Button>;
+};
+
+export default LogoutButton;
