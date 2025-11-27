a84ad5c153599d197a8925610b8cecb92ae01aa8
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-11-27 11:05:36 +0600

    show specialties management page header

--- src/app/(dashboardLayout)/admin/dashboard/specialties-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialties-management/page.tsx
@@ -1,10 +1,16 @@
+import SpecialtiesManagementHeader from "@/components/modules/Admin/SpecialtiesManagement/SpecialtiesManagementHeader";
+import RefreshButton from "@/components/shared/RefreshButton";
 
-const page = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminSpecialtiesManagementPage = () => {
+  return (
+    <div className='space-y-6'>
+      <SpecialtiesManagementHeader />
+      <div className='flex'>
+        <RefreshButton />
+      </div>
+      
+    </div>
+  );
 };
 
-export default page;
+export default AdminSpecialtiesManagementPage;
